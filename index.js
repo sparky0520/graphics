d151836f882d3cf82831657465dfd55c372fefa1
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-12-27 20:51:29 +0530

    Line connections

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function clear() {
   ctx.fillRect(0, 0, game.width, game.height);
 }
 
-// Render on screen
+// Render point on screen
 function point({ x, y }) {
   ctx.fillStyle = FOREGROUND;
   ctx.fillRect(x - size / 2, y - size / 2, size, size);
@@ -56,6 +56,16 @@ function rotate_xz({ x, y, z }, angle) {
   };
 }
 
+// Draw line b/w two points
+function connect(p1, p2) {
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = FOREGROUND;
+  ctx.beginPath();
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.stroke();
+}
+
 const FPS = 144;
 const dt = 1 / FPS;
 let dz = 1;
@@ -65,12 +75,22 @@ let angle = 0;
 const vs = [
   { x: 0.25, y: 0.25, z: 0.25 },
   { x: -0.25, y: 0.25, z: 0.25 },
-  { x: 0.25, y: -0.25, z: 0.25 },
   { x: -0.25, y: -0.25, z: 0.25 },
+  { x: 0.25, y: -0.25, z: 0.25 },
+
   { x: 0.25, y: 0.25, z: -0.25 },
   { x: -0.25, y: 0.25, z: -0.25 },
-  { x: 0.25, y: -0.25, z: -0.25 },
   { x: -0.25, y: -0.25, z: -0.25 },
+  { x: 0.25, y: -0.25, z: -0.25 },
+];
+
+const lines = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [0, 4],
+  [1, 5],
+  [2, 6],
+  [3, 7],
 ];
 
 function animate() {
@@ -78,7 +98,7 @@ function animate() {
   angle += Math.PI * dt;
   clear();
   for (v of vs) {
-    // Render
+    // Render point
     point(
       // Translate to HTML coordinates
       screen(
@@ -93,6 +113,16 @@ function animate() {
         )
       )
     );
+  }
+  for (l of lines) {
+    for (let i = 0; i < l.length; i++) {
+      const p1 = vs[l[i]];
+      const p2 = vs[l[(i + 1) % l.length]];
+      connect(
+        screen(project(translate_z(rotate_xz(p1, angle), dz))),
+        screen(project(translate_z(rotate_xz(p2, angle), dz)))
+      );
+    }
   }
   setTimeout(animate, dt);
 }
